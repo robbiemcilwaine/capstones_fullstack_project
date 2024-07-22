8353d3aa642efec91d13f02fe2ca1c08812fbcf4
Charisma <actuallycharisma@outlook.com>
2024-07-22 14:40:00 +0100

    added anchor navigation component to homepage

--- client/src/containers/ParentContainer.jsx
+++ client/src/containers/ParentContainer.jsx
@@ -1,17 +1,27 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import NavigationComponent from '../components/NavigationComponent';
+import { CssBaseline, Container, Typography } from '@mui/material';
 
 
 
 const ParentContainer = () => {
 
     return (
-        <>
-
-        </>
+    <React.Fragment>
+        <CssBaseline />
+        <Container>
+            <Typography variant="h4" component="h1" gutterBottom>
+            My Application
+            </Typography>
+            <NavigationComponent />
+        </Container>
+    </React.Fragment>
     )
 }
 
-export default function ButtonUsage() {
+export function ButtonUsage() {
     return <Button variant="contained">Hello world</Button>;
 }
+
+export default ParentContainer; 

807399288845bef923c8487a23e94bad6703b705
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-07-22 10:57:35 +0100

    add responsive mobileUI

---
+++ client/capstone_client/src/containers/ParentContainer.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+
+
+const ParentContainer = () => {
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default function ButtonUsage() {
+    return <Button variant="contained">Hello world</Button>;
+}

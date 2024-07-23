03cb3c2fb64d8bed4f078363048558b318cfb1aa
Rabin Kandel <kandelrabin@live.com>
2024-07-23 16:40:38 +0100

    revert to mvp wireframe component

--- client/src/components/DeliveryComponent.jsx
+++ client/src/components/DeliveryComponent.jsx
@@ -26,58 +26,6 @@ function preventDefault(event) {}
 
 
 export default function DeliveryComponent({waypointData}) {
-
-  const StyledMenu = styled((props) => (
-    <Menu
-      elevation={0}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'right',
-      }}
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      {...props}
-    />
-  ))(({ theme }) => ({
-    '& .MuiPaper-root': {
-      borderRadius: 6,
-      marginTop: theme.spacing(1),
-      minWidth: 180,
-      color:
-        theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-      boxShadow:
-        'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-      '& .MuiMenu-list': {
-        padding: '4px 0',
-      },
-      '& .MuiMenuItem-root': {
-        '& .MuiSvgIcon-root': {
-          fontSize: 18,
-          color: theme.palette.text.secondary,
-          marginRight: theme.spacing(1.5),
-        },
-        '&:active': {
-          backgroundColor: alpha(
-            theme.palette.primary.main,
-            theme.palette.action.selectedOpacity,
-          ),
-        },
-      },
-    },
-  }));
-  
-  function CustomizedMenus() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    }
-  }
  
   return (
     <React.Fragment>
@@ -87,12 +35,6 @@ export default function DeliveryComponent({waypointData}) {
         Generate Routes
       </Button>
     </Stack>
-    <FormControl>
-      <InputLabel htmlFor="input-name">Customer Name</InputLabel>
-      <Input id="input-name" aria-describedby="my-helper-text" />
-      <FormHelperText id="my-helper-text">Seach by Name</FormHelperText>
-      <Button variant="outline">Search</Button> 
-    </FormControl>  
 <Box>
     <Table size="small">
       <TableHead>

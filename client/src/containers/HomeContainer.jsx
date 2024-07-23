0b2fa4d4ba48aeecf6e85bc118f3976dac1337ef
Charisma <actuallycharisma@outlook.com>
2024-07-23 15:57:57 +0100

    centered the table but still not wide enough

--- client/src/containers/HomeContainer.jsx
+++ client/src/containers/HomeContainer.jsx
@@ -2,14 +2,16 @@ import Dashboard from "../components/Dashboard";
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
+import { CenterFocusStrong } from "@mui/icons-material";
 
 const HomeContainer = ({deliveryData}) => {
     return (
         <>
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                <Grid container spacing={3}>
+        <h1>Current Deliveries</h1>
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4, display: 'flex',  justifyContent: "center"}}>
+                <Grid container spacing={3} justifyContent='center'>
                     <Grid item xs={12}>
-                        <Paper sx={{ p: 10, display: 'flex', flexDirection: 'column', width: '200%', backgroundColor: 'orange'}}>
+                        <Paper sx={{p: 10, display: 'flex', flexDirection: 'column', backgroundColor: 'orange'}}>
                             <Dashboard deliveryData={deliveryData}/>
                         </Paper>
                     </Grid>

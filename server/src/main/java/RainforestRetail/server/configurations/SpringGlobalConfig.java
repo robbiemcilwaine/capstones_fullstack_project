e02c29fd0ff974b61d145c3b5e85b8474429c7a3
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-07-23 13:44:52 +0100

    add cors mapping

--- server/src/main/java/RainforestRetail/server/configurations/SpringGlobalConfig.java
+++ server/src/main/java/RainforestRetail/server/configurations/SpringGlobalConfig.java
@@ -1,2 +1,17 @@
-package RainforestRetail.server.configurations;public class SpringGlobalConfig {
+package RainforestRetail.server.configurations;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringGlobalConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+
 }

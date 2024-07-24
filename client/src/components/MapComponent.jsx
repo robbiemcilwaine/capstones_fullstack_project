1ba27d81179ce46bb6a0eed906ec99bfb31ae83e
Charisma <actuallycharisma@outlook.com>
2024-07-24 11:55:47 +0100

    added select option for all postal district, HD1, HD2 and HD8 work but others do not

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -1,46 +1,42 @@
-import {MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import "leaflet/dist/leaflet.css"; 
 import 'leaflet-routing-machine'; 
 import L from 'leaflet';
+import { useEffect } from 'react';
 
-const Map = () => {
-const center = [51.505, -0.09]; //map center
-  const position = [51.505, -0.09]; //marker 
+const Map = ({waypoints}) => {
+  const center = [53.6458, -1.7850]; // map center
 
-  const route = L.Routing.control({
-    waypoints: [
-        L.latLng(57.74, 11.94),
-        L.latLng(57.6792, 11.949)
-    ], 
-    lineOptions: {
-      styles: [
-        {
-          color: "blue",
-          opacity: 0.6,
-          weight: 4
-        }
-      ]
-    
-    }
-})
+  useEffect(() => {
+    const map = L.map('map').setView(center, 13);
 
-return (
-    <>
-      <MapContainer id='map' center={center} zoom={13} scrollWheelZoom={false}>
-        
-          <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={position}>
-              <Popup>
-              Starting point. <br />  6 Southwark St, London SE1 1TQ.
-              </Popup>
-          </Marker>
-      </MapContainer>
-    </>
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-)
+    if (waypoints.length > 0) {
+    const route = L.Routing.control({
+      waypoints: waypoints,
+      routeWhileDragging: true,
+      lineOptions: {
+        styles: [
+          {
+            color: "blue",
+            opacity: 0.6,
+            weight: 4
+          }
+        ]
+      },
+      createMarker: function() { return null; }
+    }).addTo(map);
+  }
 
+    return () => map.remove();
+  }, [center, waypoints]);
+
+  return (
+    <div id='map'></div>
+  );
 }
-export default Map; 
+
+export default Map;
